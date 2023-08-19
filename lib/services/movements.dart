f47c543ca3e33a7f6215b9b49e315095cadd6a13
Alejandro Lopez <alexjes6543@gmail.com>
2023-08-19 19:10:04 -0400

    fix movements key value misconception

--- lib/services/movements.dart
+++ lib/services/movements.dart
@@ -5,14 +5,20 @@ import '../DTO/movement_dto.dart';
 import '../utils/encryption_helper.dart';
 
 class MovementsService {
-  final EncryptionHelper _encryptionHelper = EncryptionHelper(); 
+  final EncryptionHelper _encryptionHelper = EncryptionHelper();
 
   Future<void> saveEncryptedMovement(MovementDTO movement) async {
     final jsonString = json.encode(movement.toJson());
     final encrypter = Encrypter(AES(_encryptionHelper.key, mode: AESMode.cbc));
     final encrypted = encrypter.encrypt(jsonString, iv: _encryptionHelper.iv);
-    await _encryptionHelper.storage
-        .write(key: '${movement.id}', value: encrypted.base64);
+    var movements = await _encryptionHelper.storage.read(key: 'movements');
+    if (movements == null) {
+      await _encryptionHelper.storage
+          .write(key: 'movements', value: encrypted.base64);
+    } else {
+      await _encryptionHelper.storage
+          .write(key: 'movements', value: '$movements,${encrypted.base64}');
+    }
   }
 
   Future<void> deleteEncryptedMovement(int id) async {
@@ -20,43 +26,53 @@ class MovementsService {
   }
 
   Future<void> deleteAllEncryptedMovements() async {
-    await _encryptionHelper.storage.deleteAll();
+    await _encryptionHelper.storage.delete(key: 'movements');
   }
-
 
   Future<MovementDTO?> getDecryptedMovement(int id) async {
-    try {
-      final encryptedValue = await _encryptionHelper.storage.read(key: '$id');
-      if (encryptedValue == null) return null;
+    final encryptedMovements =
+        await _encryptionHelper.storage.read(key: 'movements');
+    if (encryptedMovements != null) {
       final encrypter =
           Encrypter(AES(_encryptionHelper.key, mode: AESMode.cbc));
-      final decrypted =
-          encrypter.decrypt64(encryptedValue, iv: _encryptionHelper.iv);
-      final decodedJson = json.decode(decrypted);
-
-      return MovementDTO.fromJson(decodedJson);
-    } catch (e) {
-      return null;
-    }
-  }
-
-  Future<List<MovementDTO>> getAllDecryptedMovements() async {
-    List<MovementDTO> decryptedMovements = [];
-    final allKeys = await _encryptionHelper.storage.readAll();
-    final encrypter = Encrypter(AES(_encryptionHelper.key, mode: AESMode.cbc));
-    for (var key in allKeys.keys) {
-      final encryptedValue = allKeys[key];
-      if (encryptedValue != null) {
+      final encryptedValues = encryptedMovements.split(',');
+      for (var encryptedValue in encryptedValues) {
         try {
           final decrypted =
               encrypter.decrypt64(encryptedValue, iv: _encryptionHelper.iv);
           final decodedJson = json.decode(decrypted);
-          decryptedMovements.add(MovementDTO.fromJson(decodedJson));
+          final movement = MovementDTO.fromJson(decodedJson);
+          if (movement.id == id) {
+            return movement;
+          }
         } catch (e) {
           // Handle decryption errors
         }
       }
     }
-    return decryptedMovements;
+  }
+
+  Future<List<MovementDTO>> getAllDecryptedMovements() async {
+    final encryptedMovements =
+        await _encryptionHelper.storage.read(key: 'movements');
+    if (encryptedMovements != null) {
+      final encrypter =
+          Encrypter(AES(_encryptionHelper.key, mode: AESMode.cbc));
+      final encryptedValues = encryptedMovements.split(',');
+      final List<MovementDTO> movements = [];
+      for (var encryptedValue in encryptedValues) {
+        try {
+          final decrypted =
+              encrypter.decrypt64(encryptedValue, iv: _encryptionHelper.iv);
+          final decodedJson = json.decode(decrypted);
+          final movement = MovementDTO.fromJson(decodedJson);
+          movements.add(movement);
+        } catch (e) {
+          // Handle decryption errors
+        }
+      }
+      return movements;
+    }
+    return [];    
   }
 }
